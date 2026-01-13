22c918053b5f4a72217e4f9e93cc327cf2dfc73d
Monica Cruz <mcatcruz@gmail.com>
2026-01-12 16:43:06 -0800

    Added question.ts stub

--- src/models/question.ts
+++ src/models/question.ts
@@ -1,1 +1,25 @@
+// String Unions
+type Category = "american_government" | "american_history" | "symbols_and_holidays";
+type GradingMode = "exact" | "one_of" | "multi";
+type AnswerType = "static" | "dynamic_official" | "state_specific";
+type USCISCivicsQuestionsVersion = "2025" | "2008";
 
+interface Question {
+    id: number;
+    question_text: string;
+    acceptable_answers: string[];
+    acceptable_answers_norm?: string[];
+    is_65_20_starred: boolean;
+    category: Category;
+    grading_mode: GradingMode;
+    answer_type: AnswerType;
+    required_count?: number;
+    notes?: Note[];
+    uscis_civics_questions_version: USCISCivicsQuestionsVersion;
+}
+
+interface Note {
+    timestamp: string;
+    note: string;
+    source: string;
+}
