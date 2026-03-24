95fb208b587efc94e74e603c0081fb05907aca06
Monica Cruz <mcatcruz@gmail.com>
2026-03-24 13:38:38 -0700

    Vite + React set up

---
+++ civics-test-sim/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Session } from './components/Session';
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <Session/>
+      </section>
+
+      
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
---
+++ civics-test-sim/src/components/Session.tsx
@@ -0,0 +1,26 @@
+
+import type { Session } from '../../../src/models/session';
+import type { Question } from '../../../src/models/question';
+import type { SessionConfig } from '../../../src/models/sessionConfig';
+
+import { getCurrentQuestion } from '../../../src/session/getCurrentQuestion';
+import { createSession } from '../../../src/session/createSession';
+import { QUESTION_BANK } from '../../../src/hardcoded_data/QUESTION_BANK_2025';
+
+export function Session() {
+    // Call createSession -> store in a variable so getCurrentQuestion can access  
+        // Create SessionConfig variable to use as arg? Use QUESTION_BANK_2025 for questions
+    const sessionConfig: SessionConfig = {
+        mode: 'mock',
+        state:  'CA',
+        max_questions: 20,
+        pass_threshold: 12,
+        fail_threshold: 9,
+    }
+    const currentSession: Session = createSession(sessionConfig, QUESTION_BANK);
+    const currentQuestion: Question | null = getCurrentQuestion(currentSession)
+
+    return (
+        <h2>{currentQuestion?.question_text}</h2>
+    );
+};
